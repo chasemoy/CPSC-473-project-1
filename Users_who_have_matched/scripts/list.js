343909ec75efa00b5291f0d7cd3a4dede0f3b504
SalinasJF <SalinasJF@csu.fullerton.edu>
2018-03-29 14:57:12 -0700

    Added the functionality for two users who have matched.
    The array for two matched users will be passed to addRow(). Then Jquery will handle adding a mini dropdown menu to show the usernames and books/comments.

---
+++ Users_who_have_matched/scripts/list.js
@@ -0,0 +1,53 @@
+(function(window) {
+  "use strict";
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function CheckList(selector) {
+    if (!selector) {
+      throw new Error("No selector provided");
+    }
+    this.$element = $(selector);
+    if (this.$element.length === 0) {
+      throw new Error("Could not find element with selector: " + selector);
+    }
+}
+
+
+
+ CheckList.prototype.addRow = function(userProfile) {
+   // Create a new instance of a row, using the coffee order info
+   var rowElement = new Row(userProfile);
+   // Add the new row instance's $element property to the checklist
+   this.$element.append(rowElement.$element);
+ };
+
+ var matchcount = 3;
+ function Row(userProfile) {
+   var buttonid = "Demo"+matchcount;
+
+   var $div = $('<div></div>', {
+         'id': buttonid,
+         'class': 'w3-hide w3-container'
+   });
+
+   var name = userProfile.username;
+   var r= $('<button onclick="myFunction(\''+buttonid+'\')" class="w3-button w3-block w3-theme-l1 w3-left-align">');
+   var i = $('<i class="fa fa-users fa-fw w3-margin-right">  </i>'+name+'</button>');
+   r.append(i);
+   var $p = $('<p></p>');
+   var description = userProfile.book1+" ";
+   description += userProfile.book1comment+" ";
+   description += "<br>"+userProfile.book2+" ";
+   description += userProfile.book2comment+" ";
+   matchcount = (matchcount+1);
+
+   $p.append(description);
+   $div.append($p);
+   r.append($div);
+   $(".w3-white").append(r);
+
+ }
+ App.CheckList = CheckList;
+ window.App = App;
+})(window);
