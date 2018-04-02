921df1cdac8abe9f9be100039f08f424e4b79bba
KimSaeHun <kimsaehun@csu.fullerton.edu>
2018-04-01 21:01:38 -0700

    Fix typo and remove log statements

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -4,42 +4,40 @@
   //var RE=/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   dpd.users.me(function(me) {
     console.log(me);
-/*    me.forEach( function(element){
-      if(element==NULL || element==undefined){
-        element=" ";
-      }
-    });*/
+    /*    me.forEach( function(element){
+          if(element==NULL || element==undefined){
+            element=" ";
+          }
+        });*/
     $('#username').append("<span>" + me['username'] + "</span>");
     $('#email').attr('value', me['email']);
     $('#booktitle1').attr('value', me['bookTitle1']);
     $('#booktitle2').attr('value', me['bookTitle2']);
     $('#booktitle3').attr('value', me['bookTitle3']);
-    $('#bookcommnet1').attr('value', me['bookComment1']);
-    $('#bookcommnet2').attr('value', me['bookComment2']);
-    $('#bookcommnet3').attr('value', me['bookComment3']);
+    $('#bookcomment1').attr('value', me['bookComment1']);
+    $('#bookcomment2').attr('value', me['bookComment2']);
+    $('#bookcomment3').attr('value', me['bookComment3']);
 
   });
 
 
-$(".editemail").click(function(){
-  $("#email").attr('readonly',false);
-})
-$(".editfield1").click(function(){
-  $(".edit1").attr('readonly',false);
-})
-$(".editfield2").click(function(){
-  $(".edit2").attr('readonly',false);
-})
-$(".editfield3").click(function(){
-  $(".edit3").attr('readonly',false);
-})
+  $(".editemail").click(function() {
+    $("#email").attr('readonly', false);
+  })
+  $(".editfield1").click(function() {
+    $(".edit1").attr('readonly', false);
+  })
+  $(".editfield2").click(function() {
+    $(".edit2").attr('readonly', false);
+  })
+  $(".editfield3").click(function() {
+    $(".edit3").attr('readonly', false);
+  })
 
   //On click save this fucntion will update book info into the collection of user
   $(".profile_form").submit(function() {
     event.preventDefault();
-    console.log("hii");
     dpd.users.me(function(me) {
-      console.log("hii");
       var username = $('#username').val();
       var email = $('#email').val();
       var book1_title = $('#booktitle1').val();
@@ -48,7 +46,6 @@ $(".editfield3").click(function(){
       var book2_comment = $('#bookcomment2').val();
       var book3_title = $('#booktitle3').val();
       var book3_comment = $('#bookcomment3').val();
-      console.log(email);
       dpd.users.put(me['id'], {
         "email": email,
         "bookTitle1": book1_title,
@@ -58,14 +55,13 @@ $(".editfield3").click(function(){
         "bookTitle3": book3_title,
         "bookComment3": book3_comment
       }, function(user, err) {
-        if (err) return console.log(err);
-        console.log(user);
+        if (err) console.log(err);
       });
 
     });
-      $("#email").attr('readonly',true);
-        $(".edit1").attr('readonly',true);
-          $(".edit2").attr('readonly',true);
-            $(".edit3").attr('readonly',true);
+    $("#email").attr('readonly', true);
+    $(".edit1").attr('readonly', true);
+    $(".edit2").attr('readonly', true);
+    $(".edit3").attr('readonly', true);
   });
 })(window);
