b6a67d74a5ba71e87d44a6709012f6876f5b1dc2
KimSaeHun <kimsaehun@csu.fullerton.edu>
2018-04-01 21:35:33 -0700

    Fix proper values not showing up

--- public/scripts/swipe.js
+++ public/scripts/swipe.js
@@ -38,21 +38,22 @@
   }
 
   function displayProfile(result) {
+    console.log(result);
     $(".username").text(result.username);
 
-    var book1 = result.bookTitle1 == "" ? result.bookTitle1 : NO_BOOK_TEXT;
+    var book1 = result.bookTitle1 ? result.bookTitle1 : NO_BOOK_TEXT;
     $("#book1").text(book1);
-    var comment1 = result.bookComment1 == "" ? result.bookComment1 : NO_COMMENT_TEXT;
+    var comment1 = result.bookComment1 ? result.bookComment1 : NO_COMMENT_TEXT;
     $("#comment1").text(comment1);
 
-    var book2 = result.bookTitle2 == "" ? result.bookTitle2 : NO_BOOK_TEXT;
+    var book2 = result.bookTitle2 ? result.bookTitle2 : NO_BOOK_TEXT;
     $("#book2").text(book2);
-    var comment2 = result.bookComment2 == "" ? result.bookComment2 : NO_COMMENT_TEXT;
+    var comment2 = result.bookComment2 ? result.bookComment2 : NO_COMMENT_TEXT;
     $("#comment2").text(comment2);
 
-    var book3 = result.bookTitle3 == "" ? result.bookTitle3 : NO_BOOK_TEXT;
+    var book3 = result.bookTitle3 ? result.bookTitle3 : NO_BOOK_TEXT;
     $("#book3").text(book3);
-    var comment3 = result.bookComment3 == "" ? result.bookComment3 : NO_COMMENT_TEXT;
+    var comment3 = result.bookComment3 ? result.bookComment3 : NO_COMMENT_TEXT;
     $("#comment3").text(comment3);
   }
 
