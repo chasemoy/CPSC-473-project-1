59d44516095b63f8df02dd1bde61734279929b7d
Brian <brianquochungtruong@gmail.com>
2018-04-01 19:01:30 -0700

    Swipe page cycles through content

--- public/scripts/swipe.js
+++ public/scripts/swipe.js
@@ -1,6 +1,8 @@
-var userID;
+var userId;
 var i = 2;
+var lengthList = {};
 dpd.users.me(function(user) {
+
   if (user) {
     userId = user.id;
   } else {
@@ -8,10 +10,16 @@ dpd.users.me(function(user) {
   }
 
   dpd.users.get(function (result, err) {
-
+    console.log(lengthList);
+i = Math.floor(Math.random() * result.length);
+while (userId == result[i].id)
+{
+  i = Math.floor(Math.random() * result.length);
+}
       if (userId != result[i].id) {
+        $("h7").text(`Person   ${i}`);
         $("h1").text(`Book 1: ${  result[i].bookTitle1}`);
-        $("h2").text(`Book 1 comment : ${  result[i].bookComment1}`);
+        $("h2").text(` Book 1 comment : ${  result[i].bookComment1}`);
         $("h3").text(`Book 2: ${  result[i].bookTitle2}`);
         $("h4").text(`Book 2 comment: ${  result[i].bookComment2}`);
         $("h5").text(`Book 3: ${  result[i].bookTitle3}`);
@@ -25,10 +33,53 @@ dpd.users.me(function(user) {
   });
 });
 
-$('#dislke-btn').click(function() {
-i++;
+$('#dislike-btn').click(function() {
+  dpd.users.get(function (result, err) {
+
+    i = Math.floor(Math.random() * result.length);
+    while (userId == result[i].id)
+    {
+      i = Math.floor(Math.random() * result.length);
+    }
+      if (userId != result[i].id) {
+        $("h7").text(`Person   ${i}`);
+        $("h1").text(`Book 1: ${  result[i].bookTitle1}`);
+        $("h2").text(` Book 1 comment : ${  result[i].bookComment1}`);
+        $("h3").text(`Book 2: ${  result[i].bookTitle2}`);
+        $("h4").text(`Book 2 comment: ${  result[i].bookComment2}`);
+        $("h5").text(`Book 3: ${  result[i].bookTitle3}`);
+        $("h6").text(`Book 3 comment: ${  result[i].bookComment3}`);
+      }
+
+    if (err) {
+   return console.log(err);
+  }
+    console.log(result);
+  });
 });
 
 $('#like-btn').click(function() {
-i++;
+
+  dpd.users.get(function (result, err) {
+
+    i = Math.floor(Math.random() * result.length);
+    while (userId == result[i].id)
+    {
+      i = Math.floor(Math.random() * result.length);
+    }
+      if (userId != result[i].id) {
+        $("h7").text(`Person   ${i}`);
+        $("h1").text(`Book 1: ${  result[i].bookTitle1}`);
+        $("h2").text(` Book 1 comment : ${  result[i].bookComment1}`);
+        $("h3").text(`Book 2: ${  result[i].bookTitle2}`);
+        $("h4").text(`Book 2 comment: ${  result[i].bookComment2}`);
+        $("h5").text(`Book 3: ${  result[i].bookTitle3}`);
+        $("h6").text(`Book 3 comment: ${  result[i].bookComment3}`);
+      }
+
+    if (err) {
+   return console.log(err);
+  }
+    console.log(result);
+  });
 });
